480dc93ec70ffdcde8c8aa56177ef83d39653076
fabferri <fabferri@outlook.com>
2023-05-16 21:05:49 +0100

    modify script

--- scripts/FSBuildS2.ps1
+++ scripts/FSBuildS2.ps1
@@ -10,80 +10,32 @@
 #
 
 Param(
-  [Parameter()]
-  [string]$User2,
-  [string]$Pass2,
-  [string]$User3,
-  [string]$Pass3
+[Parameter()]
+[string]$User1,
+[string]$Pass1,
+[string]$User2,
+[string]$Pass2
 )
 
 Start-Transcript -Path "C:\Workshop\MaxFSBuildS2.log"
 
 # 1. Open Firewall for ICMP
 Write-Host "Opening ICMPv4 Port"
-Try {
-  Get-NetFirewallRule -Name Allow_ICMPv4_in -ErrorAction Stop | Out-Null
-  Write-Host "Port already open"
-}
-Catch {
-  New-NetFirewallRule -DisplayName "Allow ICMPv4" -Name Allow_ICMPv4_in -Action Allow -Enabled True -Profile Any -Protocol ICMPv4 | Out-Null
-  Write-Host "Port opened"
-}
+Try {Get-NetFirewallRule -Name Allow_ICMPv4_in -ErrorAction Stop | Out-Null
+     Write-Host "Port already open"}
+Catch {New-NetFirewallRule -DisplayName "Allow ICMPv4" -Name Allow_ICMPv4_in -Action Allow -Enabled True -Profile Any -Protocol ICMPv4 | Out-Null
+       Write-Host "Port opened"}
 
-# 2. Install IIS
-Write-Host "Installing IIS and .Net 4.5, this can take some time, around 5+ minutes..." -ForegroundColor Cyan
-Add-WindowsFeature Web-Server, Web-Mgmt-Console, Web-Asp-Net45
-
-# Configure IIS
-$WebConfig = '<?xml version="1.0" encoding="utf-8"?>
-<configuration>
-  <system.webServer>
-    <defaultDocument>
-      <files>
-        <add value="Rand.txt" />
-      </files>
-    </defaultDocument>
-  </system.webServer>
-</configuration>'
-$WebConfig | Out-File -FilePath "C:\inetpub\wwwroot\Web.config" -Encoding ascii
-
-# 3. Test/Create Folders
-Write-Host "Creating required folder"
-$Dirs = @()
-$Dirs += "C:\WebShare"
-$Dirs += "C:\inetpub\wwwroot"
-foreach ($Dir in $Dirs) {
-  If (-not (Test-Path -Path $Dir)) { New-Item $Dir -ItemType Directory | Out-Null }
-}
-
-# 4. Write out Rand.txt
-$FileContent = "Hello, I'm the contents of a remote file on $env:computername."
-$Dirs = @()
-$Dirs += "C:\WebShare"
-$Dirs += "C:\inetpub\wwwroot"
-foreach ($Dir in $Dirs) {
-  $FileContent | Out-File -FilePath "$Dir\Rand.txt" -Encoding ascii
-}
-
-
-# 5. Set Permissions on share
-$Acl = Get-Acl "C:\WebShare"
-$AccessRule = New-Object system.security.accesscontrol.filesystemaccessrule("Everyone", "ReadAndExecute, Synchronize", "ContainerInherit, ObjectInherit", "InheritOnly", "Allow")
-$Acl.SetAccessRule($AccessRule)
-Set-Acl "C:\WebShare" $Acl
-
-# 6. Create network share
-Net Share WebShare=C:\WebShare "/grant:Everyone,READ"
-
-# 7. Add additional local Admin accounts
+# 2. Add additional local Admin accounts
+# Add additional local Admin accounts
 $userList = @{}
+# if $User1 and $Pass12 are not null, then add to the hash $userList
+if (-not ([string]::IsNullOrEmpty($User1) -or [string]::IsNullOrEmpty($Pass1) )) {
+  $userList.Add($User1, $Pass1)    
+}
 # if $User2 and $Pass2 are not null, then add to the hash $userList
 if (-not ([string]::IsNullOrEmpty($User2) -or [string]::IsNullOrEmpty($Pass2) )) {
-  $userList.Add($User2, $Pass2)    
-}
-# if $User3 and $Pass3 are not null, then add to the hash $userList
-if (-not ([string]::IsNullOrEmpty($User3) -or [string]::IsNullOrEmpty($Pass3) )) {
-  $userList.Add($User3, $Pass3)
+  $userList.Add($User2, $Pass2)
 }
 
 if ($userList.Count -gt 0) {
@@ -109,6 +61,51 @@ if ($userList.Count -gt 0) {
     }
   }
 }
+
+# 3. Install IIS
+Write-Host "Installing IIS and .Net 4.5, this can take some time, around 5+ minutes..." -ForegroundColor Cyan
+Add-WindowsFeature Web-Server, Web-Asp-Net45
+
+# Configure IIS
+$WebConfig ='<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <system.webServer>
+    <defaultDocument>
+      <files>
+        <add value="Rand.txt" />
+      </files>
+    </defaultDocument>
+  </system.webServer>
+</configuration>'
+$WebConfig | Out-File -FilePath "C:\inetpub\wwwroot\Web.config" -Encoding ascii
+
+# 4. Test/Create Folders
+Write-Host "Creating required folder"
+$Dirs = @()
+$Dirs += "C:\WebShare"
+$Dirs += "C:\inetpub\wwwroot"
+foreach ($Dir in $Dirs) {
+    If (-not (Test-Path -Path $Dir)) {New-Item $Dir -ItemType Directory | Out-Null}
+}
+
+# 5. Write out Rand.txt
+$FileContent = "Hello, I'm the contents of a remote file on $env:computername."
+$Dirs = @()
+$Dirs += "C:\WebShare"
+$Dirs += "C:\inetpub\wwwroot"
+foreach ($Dir in $Dirs) {
+    $FileContent | Out-File -FilePath "$Dir\Rand.txt" -Encoding ascii
+}
+
+
+# 6. Set Permissions on share
+$Acl = Get-Acl "C:\WebShare"
+$AccessRule = New-Object system.security.accesscontrol.filesystemaccessrule("Everyone","ReadAndExecute, Synchronize","ContainerInherit, ObjectInherit","InheritOnly","Allow")
+$Acl.SetAccessRule($AccessRule)
+Set-Acl "C:\WebShare" $Acl
+
+# 7. Create network share
+Net Share WebShare=C:\WebShare "/grant:Everyone,READ"
 
 # End Nicely
 Write-Host "File Server Set up Successfull!"
